c7872bb830a05c70dbabb720b8bf81bc821d42eb
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-14 20:18:10 +0200

    test 2 green

---
+++ src/main/java/BowlingKata/BowlingKata/Game.java
@@ -0,0 +1,17 @@
+package BowlingKata.BowlingKata;
+
+public class Game {
+
+	private int score = 0;
+	
+	public void roll(int points) {
+		score += points;
+		
+	}
+
+	public int score(){
+		
+		return score;
+	}
+	
+}
