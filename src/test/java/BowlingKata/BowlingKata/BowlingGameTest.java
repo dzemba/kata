bae7ee385da5b60946986d79e4ced59a490eb146
rafal Dzierzbinski 180157 <dzemba@op.pl>
2015-05-14 20:11:12 +0200

    test 1 green + refactor

--- src/test/java/BowlingKata/BowlingKata/BowlingGameTest.java
+++ src/test/java/BowlingKata/BowlingKata/BowlingGameTest.java
@@ -1,16 +1,25 @@
 package BowlingKata.BowlingKata;
 
+
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 
 public class BowlingGameTest{
 	
 	@Test
  	public void gameWithoutPoints() {
 		Game g = new Game();
-		for(int i =0; i<20; i++){
-			g.roll(0);
-			assertThat( g.score , 0 );
-		}
+		rollAFewTimes(g,0);
+		assertThat( g.score() , is(0) );
+	}
 	
+	
+	public void rollAFewTimes(Game game, int points){
+		for(int i =0; i<20; i++){
+			game.roll(points);
+		
+		}
 	}
 } 
